5c05645079a7a56499b799c2d769d5993303729a
unknown <velimir.mueller@googlemail.com>
2022-05-25 13:50:50 +0200

    Fixed some visual bugs on small screens where buttons were rendered not in full size which simply looked odd and those buttons should not behave like that. The solution was to add a 'default' tailwind class of width-full to each button

--- components/app/index/components/HeroLarge.vue
+++ components/app/index/components/HeroLarge.vue
@@ -26,12 +26,12 @@
           <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
             <button 
               href="#" 
-              class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8"
+              class="flex w-full items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8"
               @click="openModal"
               v-text="text.button.login" />
             <button
               href="#" 
-              class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8"
+              class="flex w-full items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8"
               @closeModal="updateparent"
               v-text="text.button.register" />
           </div>
